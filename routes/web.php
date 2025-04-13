835530cc715469cd8b839482f9736b22471090cf
nayla2105 <naylachindy23@gmail.com>
2025-04-14 05:57:56 +0700

    Menyiapkan Login view dan controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListBarangController;
 use Illuminate\Support\Facades\Route;
 Use App\Http\Controllers\HomeController;
 
@@ -9,3 +10,26 @@ Use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//    return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
